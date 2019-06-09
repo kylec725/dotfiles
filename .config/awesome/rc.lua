e07132eadd7a5f11af8708e4e2ecd37df273360d
Kyle Chan <kylec725@gmail.com>
2019-06-08 22:24:17 -0400

    changed ranger default editor to nvim

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -73,7 +73,7 @@ scrot = "scrot ~/pictures/screenshots/%Y-%m-%d-%T-screenshot.png"
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
     awful.layout.suit.spiral,
-    awful.layout.suit.tile,
+    -- awful.layout.suit.tile,
     -- awful.layout.suit.fair,
     awful.layout.suit.max,
     -- awful.layout.suit.magnifier,
