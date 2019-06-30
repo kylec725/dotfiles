9ff72b3f85c825e5be5ec5e7bd2f85af8a9cba94
Kyle Chan <kylec725@gmail.com>
2019-06-29 21:39:35 -0400

    regex now accounts for multi worded network names

--- .config/awesome/bar/network.lua
+++ .config/awesome/bar/network.lua
@@ -10,12 +10,12 @@ local network = wibox.widget.textbox()
 network.text = "-----"
 
 -- local net_script = "nmcli -t -f active,ssid dev wifi | egrep '^yes' | cut -d\' -f2 | cut -c 5- | | awk '{ print $1 }'"
-local net_script = "nmcli connection show | grep wlp2s0 | awk '{ print $1 }'"
+local net_script = "nmcli connection show | grep wlp2s0"
 
 local function update_widget()
     awful.spawn.easy_async_with_shell(net_script, function(stdout)
         -- text = string.match(stdout, '%d+%%')
-        local text = stdout:match('%w+')
+        local text = stdout:match('(.+%S)%s+(%S+)  wifi')
         local icon = ""
         -- icon = ""
         if (text == nil or text == "") then
