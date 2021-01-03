a79c7080461cc8a29bfbddc35efa1251b3b283cf
Kyle Chan <kylec725@gmail.com>
2021-01-03 15:37:53 -0500

    fix default layout

--- .config/awesome/mirage.lua
+++ .config/awesome/mirage.lua
@@ -39,7 +39,7 @@ mirage.initialize = function()
 
     awful.screen.connect_for_each_screen(function(s)
 
-        awful.tag({ "一", "二", "三", "四", "五", "六", "七" }, s, awful.layout.layouts[1])
+        awful.tag({ "一", "二", "三", "四", "五", "六", "七" }, s, awful.layout.suit.spiral)
        -- for i = 1, 7, 1
        -- do
        --    awful.tag.add(i, {
