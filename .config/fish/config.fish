089b5b0336783577a3bd0f98255a9ce23d958163
Kyle Chan <kylec725@gmail.com>
2021-08-19 18:06:46 -0400

    switch to nightfox colorscheme

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -30,6 +30,41 @@ set -Ux FZF_DEFAULT_OPTS "--no-mouse --bind=tab:up,btab:down --color=prompt:195,
 set -Ux EDITOR nvim
 set -Ux VISUAL nvim
 
+# Nightfox Color Palette
+# Style: nightfox
+set -l foreground CDCECF
+set -l selection 283648
+set -l comment 526175
+set -l red c94f6d
+set -l orange F4A261
+set -l yellow dbc074
+set -l green 81B29A
+set -l purple 9D79D6
+set -l cyan 63cdcf
+set -l pink D67AD2
+
+# Syntax Highlighting Colors
+set -g fish_color_normal $foreground
+set -g fish_color_command $green
+set -g fish_color_keyword $pink
+set -g fish_color_quote $yellow
+set -g fish_color_redirection $foreground
+set -g fish_color_end $orange
+set -g fish_color_error $red
+set -g fish_color_param $purple
+set -g fish_color_comment $comment
+set -g fish_color_selection --background=$selection
+set -g fish_color_search_match --background=$selection
+set -g fish_color_operator $cyan
+set -g fish_color_escape $pink
+set -g fish_color_autosuggestion $comment
+
+# Completion Pager Colors
+set -g fish_pager_color_progress $comment
+set -g fish_pager_color_prefix $cyan
+set -g fish_pager_color_completion $foreground
+set -g fish_pager_color_description $comment
+
 # remove greeting message when fish starts
 set fish_greeting
 # remove underline of paths
